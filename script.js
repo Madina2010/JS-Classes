4df99f1d7a9f0d1ad4a239386f1e1cc54b574e7d
unknown <uznamturgul2013@outlook.com>
2023-11-28 10:07:43 +0530

    function dars

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
-// masala 1 
+// function => malum muammoni xal qilish uchun kodlar majmuosi
 
-// includes bilan
+// function types => declaration , expression , arrow
 
-let user = 'Madina';
+// declaration function
 
-if (user = user.includes('a') ) {
-    console.log(user);
-} else {
-    console.log(user);
-}
+// function example() {
+//     console.log('Salom hammaga');
+//     // return 'Salom';
+// }
 
-// toLowerCase / trim / includes bilan
+// example();  // call console bolganda
+// console.log(example()); // call return bolganda
 
-let user2 = 'Nasiba';
+/////////////////////////////////////////////////////////////////////////////////
 
-console.log(str.trim().toLowerCase().includes('n'));
+// expression yoki anonymous function
 
-// masala 2 replace bilan
+// var example = function example() {
+//     console.log('Salom hammaga');
+// }
 
-let str1 = 'Web';
-let str2 = 'Brain';
+// example();  // call
 
-console.log(str1.replace('Web','Brain'));
-console.log(str2.replace('Brain','Web'));
 
-// masala 3 if/else bilan
+/////////////////////////////////////////////////////////////////////////////////
 
-let leapyear = 2011;
+// arrow function
 
-if (leapyear % 2 == 0 || leapyear % 4 == 0) {
-    console.log('Kabisya yili');
-} else {
-    console.log('Kabisya yili emas');
-}
+// CONSOLE bilan
 
-// masala 4 replace bilan
+// var example = () => {
+//     console.log('Salom hammaga');
+// }
 
-var str = '#madina#web#brain#';
-var str = str.replace('#madina#', '(madina)').replace('#web#', 'web').replace('#brain#', '(brain)' );
-console.log(str);
+// example();
 
-// masala 5 replace va repeat bilan
+// RETURN bilan
 
-var s = 'webBrain ';
-var s = s.replace('web','IT')
-var s = s.repeat(10);
-console.log(s);
+// var example = () => 'Salom hammaga';
+
+// console.log(example());
+
+// var example = () => {return 'Salom '};
+
+// console.log(example());
+
+
+// example();  // call
+
+
+// declaration function => hoisting buladi
+// expression yoki anonymous function => hoisting bulmaydi
+// arrow function => hoisting bulmaydi , va qachonkiy {} bulmasa return keyword ni talab qilmaydi , AvtoReturn hususiyati mavjud
+
+
+// parametr va argument
+
+// function user(name, status, age) {        // name, status, age => parametr
+//     return `Name : ${name} Status : ${status}   Age : ${age}`
+// }
+
+// console.log(user('Madina ', 'developer' ,' 16 '));   // Madina , developer , 16 => argument 
+// console.log(user('Nasiba ', 'mentor' ,' 18 '));   // Nasiba , mentor , 18 => argument 
+// console.log(user('Dilorom ', 'student' ,' 13 '));   // Dilorom , student , 13 => argument 
+
+// reusability => qayta-qayta foydalanish
+
+// default parametr
+
+// function user(name = 'Test', age = 25) {        // name => parametr
+//     console.log(`Name : ${name} Age : ${age}`);
+// }
+
+// user('Madina',20);
+
+
+
