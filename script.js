fbe187082bdeb6b60dae0197a3cf66170bd9aeef
unknown <uznamturgul2013@outlook.com>
2023-11-28 07:57:17 +0530

    test

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// masala 1 
+
+// includes bilan
 
 let user = 'Madina';
 
@@ -7,7 +10,13 @@ if (user = user.includes('a') ) {
     console.log(user);
 }
 
+// toLowerCase / trim / includes bilan
 
+let user2 = 'Nasiba';
+
+console.log(str.trim().toLowerCase().includes('n'));
+
+// masala 2 replace bilan
 
 let str1 = 'Web';
 let str2 = 'Brain';
@@ -15,7 +24,7 @@ let str2 = 'Brain';
 console.log(str1.replace('Web','Brain'));
 console.log(str2.replace('Brain','Web'));
 
-
+// masala 3 if/else bilan
 
 let leapyear = 2011;
 
@@ -25,11 +34,13 @@ if (leapyear % 2 == 0 || leapyear % 4 == 0) {
     console.log('Kabisya yili emas');
 }
 
+// masala 4 replace bilan
 
 var str = '#madina#web#brain#';
 var str = str.replace('#madina#', '(madina)').replace('#web#', 'web').replace('#brain#', '(brain)' );
 console.log(str);
 
+// masala 5 replace va repeat bilan
 
 var s = 'webBrain ';
 var s = s.replace('web','IT')
