95e76e280dd4f4378816cdb7c126f133025c5907
unknown <uznamturgul2013@outlook.com>
2023-11-29 17:57:01 +0530

    comment

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function karra() {
     for (let i = 1; i <= 10; i++) {
       for (let j = 1; j <= 10; j++) {
         let result = i * j;
-        console.log(`${i} * ${j} = ${result}`);
+        console.log(`${i} x ${j} = ${result}`);
       }
+      console.log('========================================'); // ajratish uchun
     }
   }
-  
+
   karra();
   
