df76447969b00f94137c13ac313bea7fec225c74
unknown <uznamturgul2013@outlook.com>
2023-12-04 16:31:10 +0530

    masala

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-// // 1 masala
+// 1 masala
 
-// let obj = {
-//     id : 1,
-//     name: 'WebBrain'
-// }
+let obj = {
+    id : 1,
+    name: 'WebBrain'
+}
 
-// console.log(Object.keys(obj));
+console.log(Object.keys(obj));
 
-// // 2 masala
+// 2 masala
 
-// let masala = {
-//     id : 1,
-//     name: 'WebBrain'
-// }
+let masala = {
+    id : 1,
+    name: 'WebBrain'
+}
 
-// function son(obj) {
-//     for (let key in obj) {
-//         if (typeof obj[key] === 'number') {
-//            console.log(obj[key]); 
-//         }
-//     }
-// }
+function son(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'number') {
+           console.log(obj[key]); 
+        }
+    }
+}
 
-// son(masala);
+son(masala);
 
-// // 3 masala
+// 3 masala
 
 
-// let masala3 = {
-//     id: 1,
-//     name: 'WebBrain',
-//     offline: true,
-//     online: true,
-//     individual: false
-// }
+let masala3 = {
+    id: 1,
+    name: 'WebBrain',
+    offline: true,
+    online: true,
+    individual: false
+}
 
-// function bol(obj) {
-//     for (let key in obj) {
-//         if (typeof obj[key] === 'boolean') {
-//             console.log(`${key}: ${obj[key]}`);
-//         }
-//     }
-// }
+function bol(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'boolean') {
+            console.log(`${key}: ${obj[key]}`);
+        }
+    }
+}
 
-// bol(masala3);
+bol(masala3);
 
+// 4 masala
